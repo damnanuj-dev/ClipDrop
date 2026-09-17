42141991acfb73eb6e0beea8630a297b364b0261
damnanuj-dev <voidlabshub@gmail.com>
2026-09-17 09:41:52 -0700

    Add files via upload

---
+++ api/download.py
@@ -0,0 +1,12 @@
+"""
+Vercel Serverless Function Entrypoint for /api/download
+"""
+
+import os
+import sys
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+from app import app
